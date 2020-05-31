fcd52629aa79ddb89d081ee571eed68282b88ded
Jeremy Willhelm <jtwillhelm@gmail.com>
2020-05-31 17:54:37 -0400

    Add project files.

---
+++ PlayerPwr.UI/Models/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerPwr.UI.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+        public int ILvl { get; set; }
+
+        [StringLength(30)]
+        public string Spec { get; set; }
+
+        public int RealmId { get; set; }
+        public Realm Realm { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
---
+++ PlayerPwr.UI/Models/Realm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerPwr.UI.Models
+{
+    public class Realm
+    {
+        public Realm()
+        {
+            Characters = new List<Character>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<Character> Characters { get; set; }
+    }
+}
